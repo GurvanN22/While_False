c2b25bdbe0203d2bd370e2b5a92ee7a757db3846
ngurvan <gurvan.nicolas@ynov.com>
2022-09-26 20:18:19 +0200

    add variables in video branch

---
+++ Variables/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() { //Les Variables
+
+	nomber := 0 // variable int
+
+	fmt.Println(nomber)
+
+	text := "Hello World!" //variable string
+
+	fmt.Println(text)
+
+	boll := true //variable bool
+
+	fmt.Println(boll)
+
+	liste := []int{1, 2, 3} // variable liste
+
+	fmt.Println(liste)
+	//////////////////////////Fonctionnes avec la table ascii (commande console: $man ascii)
+	MyFirstByte := []byte(text) //variable byte
+
+	fmt.Println(MyFirstByte)
+
+	MyFirstRune := 'a' //variable rune
+
+	fmt.Println(MyFirstRune)
+}
